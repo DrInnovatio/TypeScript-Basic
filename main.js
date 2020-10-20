a39130d8e43d278fd039bd39020094ffaa244882
DrInnovatio <turing1723@gmail.com>
2020-10-20 13:22:01 +1000

    get this lecture done.

--- main.js
+++ main.js
@@ -10,21 +10,32 @@
 //     console.log("Finally : " + i);
 // };
 // doSomething();
-var a;
-var b;
-var c;
-var d;
-var e = [1, 2, 3, 4, 5];
-var f = [1, true, 'df', false];
-var colorRed = 0;
-var colorGreen = 1;
-var colorBlue = 2;
-var Color;
-(function (Color) {
-    Color[Color["Red"] = 0] = "Red";
-    Color[Color["Green"] = 1] = "Green";
-    Color[Color["Blue"] = 2] = "Blue";
-    Color[Color["Purple"] = 3] = "Purple";
-})(Color || (Color = {}));
-;
-var backgroundColor = Color.Red;
+// let a: number;
+// let b: boolean;
+// let c: string;
+// let d: any;
+// let e: number[] = [1,2,3];
+// let f: any[] = [1, true, 'df', false];
+// const colorRed = 0;
+// const colorGreen = 1;
+// const colorBlue = 2;
+// enum Color {Red = 0, Green = 1, Blue = 2, Purple = 3};
+// let backgroundColor = Color.Red;
+// let message;
+// message = 'abc';
+// let endsWithC = (<string>message).endsWith('c');
+// let alternativeWay = (message as string).endsWith('c');
+var Point = /** @class */ (function () {
+    function Point() {
+    }
+    Point.prototype.draw = function () {
+        console.log('X: ' + this.x + ', Y: ' + this.y);
+    };
+    Point.prototype.getDistance = function (another) {
+    };
+    return Point;
+}());
+var point = new Point();
+point.x = 1;
+point.y = 2;
+point.draw();
--- main.ts
+++ main.ts
@@ -3,6 +3,8 @@
 // };
 // var message = "First TypeScript!!";
 // log(message);
+import { Point } from './point';
+
 
 // function doSomething(){
 
@@ -14,16 +16,60 @@
 
 // doSomething();
 
-let a: number;
-let b: string;
-let c: boolean;
-let d: any;
-let e: number[] = [1,2,3,4,5];
-let f: any[] = [1, true, 'df', false];
+// let a: number;
+// let b: boolean;
+// let c: string;
+// let d: any;
+// let e: number[] = [1,2,3];
+// let f: any[] = [1, true, 'df', false];
 
-const colorRed = 0;
-const colorGreen = 1;
-const colorBlue = 2;
+// const colorRed = 0;
+// const colorGreen = 1;
+// const colorBlue = 2;
 
-enum Color {Red = 0, Green = 1, Blue = 2, Purple = 3};
-let backgroundColor = Color.Red;
+// enum Color {Red = 0, Green = 1, Blue = 2, Purple = 3};
+// let backgroundColor = Color.Red;
+
+// let message;
+// message = 'abc';
+// let endsWithC = (<string>message).endsWith('c');
+// let alternativeWay = (message as string).endsWith('c');
+
+// class Point{
+
+//   // two fields and one method : three members
+
+//   // private x: number;
+//   // private _y: number;
+//   // x: number;
+//   // _y: number;
+
+//   // ? means Optional.
+
+//   constructor(private _x?: number, private _y?:number){
+//     // this._x = _x;
+//     // this._y = _y;
+//   }
+
+//   draw(){
+//     console.log('X: ' + this._x + ', Y: ' + this._y);
+//   }
+
+//   get x(){
+//     return this._x;
+//   }
+
+//   set x(value) {
+//     if (value < 0)
+//         throw new Error ('Value Cannot be less than 0.');
+//     this._x = value;
+//   }
+// }
+
+// let point = new Point(1, 2);
+// let x = point.x;  
+// point.x = 10;
+// point.draw();
+
+let point = new Point(1, 2);
+point.draw();
